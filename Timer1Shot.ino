2973c9adc3fff43e6567ae164875c187c4c5d7a5
John <smellsofbikes@users.noreply.github.com>
2015-12-26 18:54:00 -0700

    register update and test
    Fixed some confused ocr1a/b register settings and tested it on actual hardware.
    It functions correctly as a one-shot.  The minimum shot time is 66nS, as measured on a scope.  However, handing it 60,000 does not result in a 90% duty cycle, as I would have expected.  Still working on it.

--- Timer1Shot.ino
+++ Timer1Shot.ino
@@ -1,37 +1,30 @@
 
-#define OSP_SET_WIDTH(cycles) (OCR1A = 0xffff-(cycles-1))
+#define OSP_SET_WIDTH(cycles) (OCR1B = 0xffff-(cycles-1))
 
 // Setup the one-shot pulse generator and initialize with a pulse width that is (cycles) clock counts long
 
-void osp_setup(uint16_t cycles) {
-  pinMode(9, OUTPUT);
+void osp_setup(uint16_t cycles) 
+{
   pinMode(10, OUTPUT);
 
   TCCR1B = 0;    // Halt counter by setting clock select bits to 0 (No clock source).
-  TCCR1A = 0;    // This keeps anyhting from happening while we get set up
-
+                 // This keeps anyhting from happening while we get set up
   TCNT1 = 0;     // Start counting at bottom.
-   
-  OCR1B = 0;  // Set TOP to 0. This effectively keeps us from counting because the counter just keeps reseting back to 0.
+  //OCR1B = 0;  // Set TOP to 0. This effectively keeps us from counting because the counter just keeps reseting back to 0.
   OCR1A = 0;  // We break out of this by manually setting the TCNT higher than 0, in which case it will count all the way up to MAX and then overflow back to 0 and get locked up again.
   OSP_SET_WIDTH(cycles);    // This also makes new OCR values get loaded frm the buffer on every clock cycle. 
 
-  TCCR1A = _BV(COM1A1) | _BV(COM1B0) | _BV(COM1B1) | _BV(WGM10) | _BV(WGM11); // OC1B=Set on Match, clear on BOTTOM. Mode 15 Fast PWM.
+  TCCR1A = _BV(COM1B1) | _BV(COM1B0) | _BV(WGM10) | _BV(WGM11); // OC1B=Set on Match, clear on BOTTOM. Mode 15 Fast PWM.
   TCCR1B = _BV(WGM12)| _BV(WGM13) | _BV(CS10);         // Start counting now. WGM22=1 to select Fast PWM mode 15
-  
- }
+}
 
 // Setup the one-shot pulse generator
 
-void osp_setup() {
-
-  osp_setup(1);
-
-}
+void osp_setup() { osp_setup(1); }
 
 // Fire a one-shot pulse. Use the most recently set width. 
 
-#define OSP_FIRE() (TCNT1 = OCR1A - 1)
+#define OSP_FIRE() (TCNT1 = OCR1B - 1)
 
 // Test there is currently a pulse still in progress
 
@@ -41,21 +34,16 @@ void osp_setup() {
 // Order of operations in calculating m must avoid overflow of the unint16_t.
 // TCNT1 starts one count lower than the match value becuase the chip will block any compare on the cycle after setting a TCNT. 
 
-#define OSP_SET_AND_FIRE(cycles) { uint16_t m = 0xffff-(cycles-1); OCR1A=m;TCNT1= m-1;}
+#define OSP_SET_AND_FIRE(cycles) { uint16_t m = 0xffff-(cycles-1); OCR1B=m;TCNT1= m-1;}
 
-
-void setup()
-{
-  osp_setup();
-}
+void setup() { osp_setup(); }
 
 void loop()
 {
-  // Step though 0-19 cycle long pulses for demo purposes 
-
-  for (uint16_t o = 0; o < 10000; o+=100) {
+  for (uint16_t o = 0; o < 60000; o+=500) 
+  {
     OSP_SET_AND_FIRE(o);
     while (OSP_INPROGRESS());         // This just shows how you would wait if necessary - not necessary in this application. 
-    delay(1000);      // Wait a sec to let the audience clap
+    delay(100);      // Wait a sec to let the audience clap
   }
 }
